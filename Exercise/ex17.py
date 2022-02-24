5610f13e105d632d6ef68feaf87fe34363bb5fb2
Syed Atef Alvi <atefalvi@gmail.com>
2022-02-23 23:33:25 -0500

    Ex17

--- Exercise/ex17.py
+++ Exercise/ex17.py
@@ -5,6 +5,13 @@ script, from_file, to_file = argv
 
 print("Copying content from {from_file} to {to_file}")
 
+print(f"Does output file exist: {exists(to_file)}")
+
 data = open(from_file).read()
 
-print(data)
+open(to_file, 'w').write(data)
+
+print("Copy complete")
+
+open(from_file).close()
+open(to_file).close()
